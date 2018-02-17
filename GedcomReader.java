40c3ec004b974a7a3fa8622e08a67825f7463779
Siyuanhee <33169753+Siyuanhee@users.noreply.github.com>
2018-02-17 14:58:44 -0500

    Add files via upload

---
+++ GedcomReader.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class GedcomReader {
+
+	public static void main(String[] args) {
+
+	}
+}
+	
+class Individuals{
+		
+}
+	
+class Families{
+		
+}
+	
+class GED{
+		
+}
