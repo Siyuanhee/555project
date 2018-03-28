f784c9a324c63a89923f79c86cd6fefe309747c1
Siyuanhee <33169753+Siyuanhee@users.noreply.github.com>
2018-03-28 17:58:51 -0400

    Update GED.java

--- GED.java
+++ GED.java
@@ -43,6 +43,8 @@ public class GED {
         birthBeforeMarriage ();//US02
         birthBeforeDeath();//US03
         marriageBeforeDivorce();//US04
+	birthBeforeMarriageOfParents();//US08
+        parentsNotTooOld();//US12
     }
     
     public void traversal() throws FileNotFoundException, IOException, ParseException {
@@ -530,7 +532,74 @@ public class GED {
             System.err.println(e.toString());
         }
     }
+	
+    private void birthBeforeMarriageOfParents() { //US08
+        Iterator<Map.Entry<String, Individual>> indIt = individuals.entrySet().iterator();
+        Calendar birth = Calendar.getInstance();
+        Calendar divorced = Calendar.getInstance();
+
+        try {
+            while (indIt.hasNext()) {
+                Map.Entry<String, Individual> indEnt = indIt.next();
+                Iterator<String> famcIt = indEnt.getValue().getFAMC().iterator();
+
+                while (famcIt.hasNext()) {
+                    String str = famcIt.next();
+                    birth.setTime(indEnt.getValue().getBirthday());
+                    divorced.setTime(families.get(str).getMarried());
+
+                    if (families.get(str).getMarried() == null) {
+
+                    }
+                    else if (indEnt.getValue().getBirthday() == null) {
+
+                    }else if (families.get(str).getDivorced() == null) {
+
+                    }
+                    else if (families.get(str).getMarried().after(indEnt.getValue().getBirthday()))
+                        errors.add("Error US08: Birthday of " + indEnt.getValue().getName() + "(" + indEnt.getValue().getID() + ") in the family of " + families.get(str).getID() + " is before the marriage of parents.");
+                    else if (((birth.get(Calendar.YEAR) - divorced.get(Calendar.YEAR))*12 + (birth.get(Calendar.MONTH) - divorced.get(Calendar.MONTH)) > 9))
+                        errors.add("Error US08: Birthday of " + indEnt.getValue().getName() + "(" + indEnt.getValue().getID() + ") in the family of " + families.get(str).getID() + " is more than 9 months after parents divorce.");
+                }
+            }
+        } catch (Exception e) {
+            System.err.println(e.toString());
+        }
+    }
     
+    private void parentsNotTooOld() { //US12
+        Iterator<Map.Entry<String, Family>> famIt = families.entrySet().iterator();
+        Calendar husbandBirth = Calendar.getInstance();
+        Calendar wifeBirth = Calendar.getInstance();
+        Calendar childBirth = Calendar.getInstance();
+
+        try {
+            while (famIt.hasNext()) {
+                Map.Entry<String, Family> famEnt = famIt.next();
+                Iterator<String> chIt = famEnt.getValue().getChildren().iterator();
+
+                while (chIt.hasNext()) {
+                    String str = chIt.next();
+                    husbandBirth.setTime(individuals.get(famEnt.getValue().getHusbandID()).getBirthday());
+                    wifeBirth.setTime(individuals.get(famEnt.getValue().getWifeID()).getBirthday());
+                    childBirth.setTime(individuals.get(str).getBirthday());
+
+                    if (famEnt.getValue().getHusbandID() == null) {
+                    }
+                    else if(childBirth.get(Calendar.YEAR) - husbandBirth.get(Calendar.YEAR) >= 80 ){
+                    	errors.add("Error US12: Age of father " + famEnt.getValue().getHusbandName() + "(" + famEnt.getValue().getHusbandID() + ") is more than 80 years older than his children " + individuals.get(str).getName() + "(" + individuals.get(str).getID() + ").");
+                    }
+                    
+                    if (famEnt.getValue().getWifeID() == null) {
+                    }else if(childBirth.get(Calendar.YEAR) - wifeBirth.get(Calendar.YEAR) >= 60){
+                    	errors.add("Error US12: Age of mother " + famEnt.getValue().getWifeName() + "(" + famEnt.getValue().getWifeID() + ") is more than 60 years older than her children " + individuals.get(str).getName() + "(" + individuals.get(str).getID() + ").");
+                    }
+                }
+            }
+        } catch (Exception e) {
+            System.err.println(e.toString());
+        }
+    }
     
     /**
      *check errors by user stories, and then print all errors
