054395320ff9868885092b65022f2766aba9cc3d
xli119567 <35972960+xli119567@users.noreply.github.com>
2018-03-28 11:41:21 -0400

    Update userStory11.java

--- userStory11.java
+++ userStory11.java
@@ -20,7 +20,7 @@
 
                 for (Map.Entry<Date, Date> entry: marriage.entrySet()){
                     for (Date key: marriage.keySet()){
-                        if (key.after(entry.getKey()) && key.before(entry.getValue())){
+                        if (key.after(entry.getKey()) && key.before(entry.getValue())){ //判断某结婚日期是否在结婚日期和离婚日期之间
                             errors.add("Error: US11: " + indEnt.getValue().getName() + "has bigamy!");
                         }
                     }
