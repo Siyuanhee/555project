3d65e7eb518f3e93b158aeab397389d228df908a
xli119567 <35972960+xli119567@users.noreply.github.com>
2018-03-28 00:07:30 -0400

    Create userStory11.java

---
+++ userStory11.java
@@ -0,0 +1,34 @@
+    private void noBigamy() { //US11
+        Date now = new Date();
+        Iterator<Map.Entry<String, Individual>> indIt = individuals.entrySet().iterator();
+
+        try {
+            while (indIt.hasNext()){
+                Map.Entry<String, Individual> indEnt = indIt.next();
+                Iterator<String> spIt = indEnt.getValue().getFAMS().iterator();
+                Map<Date, Date> marriage = new HashMap<>();
+
+                while (spIt.hasNext()){
+                    String str = spIt.next();
+                    if (families.get(str).getDivorced() == null){
+                        marriage.put(families.get(str).getMarried(), now);
+                    }
+                    else{
+                        marriage.put(families.get(str).getMarried(),families.get(str).getDivorced());
+                    }
+                }
+
+                for (Map.Entry<Date, Date> entry: marriage.entrySet()){
+                    for (Date key: marriage.keySet()){
+                        if (key.after(entry.getKey()) && key.after(entry.getValue())){
+                            errors.add("Error: US11: " + indEnt.getValue().getName() + "has bigamy!");
+                        }
+                    }
+                }
+
+            }
+
+        }catch (Exception e) {
+            System.out.println(e.toString());
+        }
+    }
