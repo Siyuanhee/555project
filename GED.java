85306fb450822a56c11aeb41709b501707090734
Siyuanhee <33169753+Siyuanhee@users.noreply.github.com>
2018-03-28 19:39:12 -0400

    Update GED.java

--- GED.java
+++ GED.java
@@ -195,6 +195,32 @@ public class GED {
             return -1;
         }
     }
+	
+	private static int getAgeByBirth(Date birthday) {
+	    int age = 0;
+	    try {
+		Calendar now = Calendar.getInstance();
+		now.setTime(new Date());
+			
+		Calendar birth = Calendar.getInstance();
+			
+		birth.setTime(birthday);
+			
+		if (birth.after(now)) {
+			   age = 0;
+		 } else {
+		       age = now.get(Calendar.YEAR) - birth.get(Calendar.YEAR);
+		       if (now.get(Calendar.DAY_OF_YEAR) < birth.get(Calendar.DAY_OF_YEAR)) {
+			   age -= 1;
+		       }
+		}
+		    return age;
+		} catch (Exception e) {
+		    System.err.println(e.toString());
+		    return -1;
+		}
+	}
+	  
     
     public void individualsPrint() {
         String indId, nam, gend, indBirthday, age, alive, death, child, spouse;
